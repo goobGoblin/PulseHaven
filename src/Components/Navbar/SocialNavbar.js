89077b3ec957283e3a888d142619f63727240b10
goobGoblin <erb.zai@gmail.com>
2024-05-16 19:10:11 -0500

    Centered Header Styling

--- src/Components/Navbar/SocialNavbar.js
+++ src/Components/Navbar/SocialNavbar.js
@@ -3,14 +3,22 @@ import './SocialNavbar.css'; // Importing the CSS for styling
 
 function SocialNavbar() {
     return (
-        <div className="social-navbar">
+        <div class="social-navbar">
             <a href="https://soundcloud.com/yourusername" target="_blank" rel="noopener noreferrer">
                 <img src="/PulseHaven/Logos/Soundcloud.png" alt="SoundCloud" />
             </a>
             <a href="https://instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
                 <img src="/PulseHaven/Logos/Instagram.png" alt="Instagram" />
             </a>
+            <a href="https://youtube.com/yourusername" target="_blank" rel="noopener noreferrer">
+                <img src="/PulseHaven/Logos/Youtube.png" alt="Youtube" />
+            </a>
+            <a href="https://twitch.com/yourusername" target="_blank" rel="noopener noreferrer">
+                <img src="/PulseHaven/Logos/Twitch.png" alt="Twitch" />
+            </a>
         </div>
+
+        
     );
 }
 
